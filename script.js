810e9c81b669a9e8d8a5b52bd4084c55baa63a79
LIVETV10124 <salinadabvg@gmail.com>
2024-12-31 00:19:04 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function openPlayer(playerId, source, cookie) {
+    // Display modal or load player
+    alert(`Opening player for ${playerId} with source: ${source}`);
+    // Future implementation for a modal or new page player
+}
