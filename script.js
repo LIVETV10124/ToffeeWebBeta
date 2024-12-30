dfc2988fd0a9b8bdf0e88f505e76388036bca336
LIVETV10124 <salinadabvg@gmail.com>
2024-12-31 00:29:57 +0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function openPlayer(playerId, source, cookie) {
-    // Display modal or load player
-    alert(`Opening player for ${playerId} with source: ${source}`);
-    // Future implementation for a modal or new page player
-}
